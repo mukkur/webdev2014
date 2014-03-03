189b69fd8b8da0a7aec9083074f95b1011f5cf3a
mukkur <rajasekharm4u@gmail.com>
2014-03-03 09:13:35 -0500

    l

--- Final/Views/Users/index.php
+++ Final/Views/Users/index.php
@@ -1,4 +1,23 @@
 
-<pre>
-	<? print_r($model);?>
-</pre>
+
+   <table class="table">
+   	    <thead>
+   	 	    <tr>
+   	 		   <th>FirstName</th>
+   	 		   <th>LirstName</th>
+   	 		   <th>password</th>
+   	 		   <th>fbid</th>
+   	 		   <th>UserType</th>
+   	 	   </tr>
+   	   </thead>
+   	   <body>
+   	 	
+   	 	   <? foreach ($model as $row): ?>
+   	 	   <? endforeach; ?>
+   	 	      <tr>
+   	 		
+   	 	      </tr>
+   	 	
+   	   </body>
+	
+   </table>
