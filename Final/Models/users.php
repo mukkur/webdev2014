9eb10190023ee2c928039f705407bfc1bcc22fde
mukkur <rajasekharm4u@gmail.com>
2014-03-03 16:44:28 -0500

    s

--- Final/Models/users.php
+++ Final/Models/users.php
@@ -1,13 +1,13 @@
 <?php
-	include_once __DIR__.'/../Models/Users.php';
+	include_once __DIR__ .'/../inc/functions.php';
 	 
 	class Users  {
 			static public  function Get($id = null)
 			{
 				if($id == null){
 					// Get all records
-					
-					return fetch_all("SELECT * FROM USERS");
+					print_r("all");
+					return fetch_all("SELECT * FROM 2014Spring_Users");
 				}else{
 					// Get an record
 				}
