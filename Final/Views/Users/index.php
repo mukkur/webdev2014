525452a2b09b939553a038b32a7e7aaf26e3bfd9
mukkur <rajasekharm4u@gmail.com>
2014-03-06 09:11:18 -0500

    de

--- Final/Views/Users/index.php
+++ Final/Views/Users/index.php
@@ -1,23 +1,38 @@
 
 
-   <table class="table">
-   	    <thead>
-   	 	    <tr>
-   	 		   <th>FirstName</th>
-   	 		   <th>LirstName</th>
-   	 		   <th>password</th>
-   	 		   <th>fbid</th>
-   	 		   <th>UserType</th>
-   	 	   </tr>
-   	   </thead>
-   	   <body>
-   	 	
-   	 	   <? foreach ($model as $row): ?>
-   	 	   <? endforeach; ?>
-   	 	      <tr>
-   	 		
-   	 	      </tr>
-   	 	
-   	   </body>
+    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/datatables/1.9.4/css/jquery.dataTables.min.css" />
+    <h2>
+		List of Users
+	</h2>
+	<table class="table table-striped table-bordered table-hover">
+		<thead>
+			<tr>
+				<th>FirstName</th>
+				<th>LastName</th>
+				<th>Password</th>
+				<th>fbid</th>
+				<th>UserType</th>
+			</tr>
+		</thead>
+		<tbody>
+
+			<? foreach ($model as $row): ?>
+				<tr>
+					<td><?=$row['FirstName']?></td>
+					<td><?=$row['LastName']?></td>
+					<td><?=$row['Password']?></td>
+					<td><?=$row['fbid']?></td>
+					<td><?=$row['UserType']?></td>
+				</tr>
+			<? endforeach; ?>
+			
+		</tbody>
+	</table>
 	
-   </table>
+   </table> 
+   <? function JavaScripts(){?>
+   	<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/datatables/1.9.4/jquery.dataTables.min.js"></script>
+    <script type="text/javascript">
+    	$(".table").dataTable()
+    </script>
+    <?}?>
