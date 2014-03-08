79549376e007bd1db0db71f16c7eb6ab84e8df5f
mukkur <rajasekharm4u@gmail.com>
2014-03-08 11:38:19 -0500

    g

--- Final/Controllers/Users.php
+++ Final/Controllers/Users.php
@@ -1,10 +1,9 @@
 <?php
 	include_once __DIR__ . '/../inc/functions.php';
 	include_once __DIR__ . '/../inc/allModels.php';
-    print_r("1");
+
 	@$view = $action = $_REQUEST['action'];
 	@$format = $_REQUEST['format'];
-	print_r("2");
 
 	switch ($action){
 		case 'create':
@@ -13,27 +12,17 @@
 			break;
 		case 'delete':
 			break;
-	
 		default:
-			print_r("3");
-			
 			$model = Users::Get();
-			if($view === null)$view = 'index';
-			
+			if($view == null) $view = 'index';
 	}
-	switch (variable) {
+
+	switch ($format) {
 		case 'plain':
-			print_r("4");
-			
-		    include __DIR__ . "/../Views/Users/$view.php";	
+			include __DIR__ . "/../Views/Users/$view.php";			
 			break;
-		
 		default:
-			print_r("5");
-			
-			$view = __DIR__ . "/../Views/Users/$view.php";
-			print_r("6");
-			
+			$view = __DIR__ . "/../Views/Users/$view.php";	
 			include __DIR__ . "/../Views/Shared/_Layout.php";
 			break;
 	}
