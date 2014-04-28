2f80e5713439f0883a077fc15ae1dc439d33062b
unknown <mukkur1@.active.newpaltz.edu>
2014-04-28 09:09:40 -0400

    fifa

--- Final/inc/allModels.php
+++ Final/inc/allModels.php
@@ -1,2 +1,6 @@
 <?php
-	include_once __DIR__ . '/../Models/Users.php';
+include_once __DIR__ . '/../Models/Users.php';
+include_once __DIR__ . '/../Models/Keywords.php';
+include_once __DIR__ . '/../Models/Products.php';
+include_once __DIR__ . '/../Models/Accounts.php';
+
--- Final/inc/functions.php
+++ Final/inc/functions.php
@@ -1,20 +1,33 @@
 <?php
-	include_once __DIR__ . 'password.php';
+include_once __DIR__ . '/password.php';
 
-	function GetConnection(){
-		
+function GetConnection(){
+global $password;
 
-		$conn = mysqli_connect('localhost', 'mukkur1', $password, 'mukkur1_db');
-		return $conn;
-	}
-	function fetch_all($sql){
-		$conn = GetConnection();
-					$results = $conn->query("$sql");
-					$arr = array();
-					while($row = $results->fetch_assoc()){
-						$arr[] = $row;
-					}
-					$conn->close();
-					
-					return $arr;
-	}
+$conn = mysqli_connect('localhost', 'plotkinm', $password, 'plotkinm_db');
+return $conn;
+}
+
+function fetch_all($sql){
+$conn = GetConnection();
+$results = $conn->query($sql);
+$error = $conn->error;
+if($error) return $error;
+
+$arr = array();
+
+while ($row = $results->fetch_assoc()) {
+$arr[] = $row;
+}
+
+$conn->close();
+
+return $arr;
+}
+function escape_all($row, $conn){
+$row2 = array();
+foreach ($row as $key => $value) {
+$row2[$key] = $conn->real_escape_string($value);
+}
+return $row2;
+}
